a801b97570bcabb762b1b547f4cbc3cc7c84385b
Roman Komkov <r.komkov@gmail.com>
2016-03-03 02:26:52 +0100

    Separate zsh settings from prezto

---
+++ .zsh/custom.zsh
@@ -0,0 +1,27 @@
+export PATH=/usr/local/mysql/bin:$PATH
+export LC_CTYPE="en_US.UTF-8"
+export WORKON_HOME=~/.virtualenvs
+export EDITOR="vim"
+export LESSOPEN="| /usr/local/bin/src-hilite-lesspipe.sh %s"
+export LESS=" -R "
+export ANSIBLE_NOCOWS=1
+export HOMEBREW_GITHUB_API_TOKEN="a110e4ac10373697a0734fe62bde6eec3dfd7885"
+
+# Pretty colors
+export TERM='xterm-256color'
+
+bindkey '^[[Z' reverse-menu-complete
+
+# Set vi mode
+#bindkey -v
+
+# Tun on bash style incremental search
+bindkey '^R' history-incremental-search-backward
+
+source /usr/local/share/python/virtualenvwrapper.sh
+if which pyenv > /dev/null; then eval "$(pyenv init - zsh)"; fi
+
+eval "$(thefuck --alias)"
+eval "$(fasd --init posix-alias zsh-hook)"
+
+[ -f ~/.fzf.zsh  ] && source ~/.fzf.zsh
