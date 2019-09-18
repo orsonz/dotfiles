bfba42820af4e781819d4dc190aeb08f0362dde6
Uģis Ornicāns <Ugis.Ornicans@4finance.com>
2019-09-18 11:55:06 +0300

    added my aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -14,3 +14,8 @@ alias t='true'
 if command -v nvim > /dev/null 2>&1; then
   alias vim='nvim'
 fi
+
+alias go-to-ansible="source ~/.aws-credentials && cd ~/Stash/ansible/utils && source venv-init.sh && cd .."
+alias go-to-ansible-provision="source ~/.aws-credentials && cd ~/Stash/ansible/utils && source venv-init.sh provision && cd .."
+alias go-to-ansible-custom="source ~/.aws-credentials && cd ~/Stash/ansible-custom-br/utils && source venv-init.sh && cd .."
+alias go-to-ansible-custom-provision='source ~/.aws-credentials && cd ~/Stash/ansible-custom-br/utils && source venv-init.sh provision && cd ..'
