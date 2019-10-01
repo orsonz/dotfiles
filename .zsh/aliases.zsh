c7a3805e58602ee350cb3c81d9822a90dde96c5a
Roman Komkov <r.komkov@gmail.com>
2019-10-01 12:00:36 +0200

    [brew] Keep global file

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,4 +1,4 @@
-alias brewdump='brew bundle dump --force'
+alias brewdump='brew bundle dump --force --global'
 alias c='zz'
 alias cat='bat -np --paging=never --theme base16'
 alias diff='icdiff -N'
