9a8953f033e062dc13c0fab575513aae391405f9
Roman Komkov <r.komkov@gmail.com>
2019-10-15 00:22:08 +0200

    [yadm] Put functions into lib.sh

---
+++ .yadm/lib.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+
+function install_brew() {
+  if ! command -v brew >/dev/null 2>&1; then
+    echo "Installing homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+
+  if [ -f "$HOME/.Brewfile" ]; then
+    echo "Updating homebrew bundle"
+    brew bundle --global
+    # controll yadm with brew
+    brew link --overwrite yadm
+  fi
+}
+
+function install_npm() {
+  local NPM_PACKAGES=(
+    bash-language-server
+    dockerfile-language-server-nodejs
+    fixjson
+    neovim
+    prettier
+    yaml-language-server
+  )
+
+  if command -v npm >/dev/null 2>&1; then
+    for package in "${NPM_PACKAGES[@]}"; do
+      npm install -g "$package"
+    done
+  fi
+}
+
+function install_fonts() {
+  cp "$HOME"/.fonts/* "$HOME/Library/Fonts/"
+}
